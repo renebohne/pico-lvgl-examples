4a8db8b0398a595c793e30a7e2496c9c3b3032b6
renebohne <rene.bohne@gmail.com>
2023-07-04 12:08:25 +0200

    initial commit

---
+++ include/config.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#define PIN_BACKLIGHT 13
+#define PIN_BUTTON_A 15
+#define PIN_BUTTON_B 17 //DTR on SIM7080 BOARD
+#define PIN_STICK_PRESS 3
+#define PIN_STICK_UP 2
+#define PIN_STICK_DOWN 18
+#define PIN_STICK_LEFT 16
+#define PIN_STICK_RIGHT 20
+
+#define TFT_DC 8
+#define TFT_CS 9
+#define TFT_SCLK 10
+#define TFT_MOSI 11
+#define TFT_RST 12
+#define TFT_BACKLIGHT 13
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,144 @@
+#include <Arduino.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+#include <SPI.h>
+
+#include "config.hpp"
+
+#include <Adafruit_LvGL_Glue.h> // Always include this BEFORE lvgl.h!
+#include <lvgl.h>
+
+Adafruit_ST7789 tft = Adafruit_ST7789(&SPI1, TFT_CS, TFT_DC, TFT_RST);
+Adafruit_LvGL_Glue glue;
+
+void setupSPI()
+{
+    SPI1.setTX(TFT_MOSI);
+    SPI1.setSCK(TFT_SCLK);
+    SPI1.setCS(TFT_CS);
+    SPI1.begin();
+}
+
+
+void lvgl_setup(void)
+{
+    // Create simple label centered on screen
+    lv_obj_t *label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "digitale-dinge.de");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+}
+
+void setup(void)
+{
+    Serial.begin(115200);
+    delay(1000);
+    setupSPI();
+
+    Serial.println("INIT");
+
+    pinMode(PIN_STICK_DOWN, INPUT_PULLUP);
+    pinMode(PIN_STICK_UP, INPUT_PULLUP);
+    pinMode(PIN_STICK_LEFT, INPUT_PULLUP);
+    pinMode(PIN_STICK_RIGHT, INPUT_PULLUP);
+    pinMode(PIN_STICK_PRESS, INPUT_PULLUP);
+
+    pinMode(PIN_BUTTON_A, INPUT_PULLUP);
+    pinMode(PIN_BUTTON_B, INPUT_PULLUP);
+
+    tft.init(135, 240);
+    tft.setRotation(3);
+    tft.fillScreen(ST77XX_BLACK);
+    // tft.setSPISpeed(60000000);
+
+    pinMode(TFT_BACKLIGHT, OUTPUT);
+    digitalWrite(TFT_BACKLIGHT, HIGH);
+
+    LvGLStatus status = glue.begin(&tft, false);
+
+    /*
+
+    if (status != LVGL_OK)
+    {
+        Serial.printf("Glue error %d\r\n", (int)status);
+        for (;;)
+            ;
+    }
+    */
+
+    lvgl_setup(); // Call UI-building function above
+}
+
+void loop()
+{
+    if (digitalRead(PIN_STICK_DOWN) == LOW)
+    {
+        Serial.println("DOWN");
+
+        while (digitalRead(PIN_STICK_DOWN) == LOW)
+        {
+            delay(50);
+        }
+    }
+    else if (digitalRead(PIN_STICK_UP) == LOW)
+    {
+        Serial.println("UP");
+
+        while (digitalRead(PIN_STICK_UP) == LOW)
+        {
+            delay(50);
+        }
+    }
+    else if (digitalRead(PIN_STICK_LEFT) == LOW)
+    {
+        Serial.println("LEFT");
+
+        while (digitalRead(PIN_STICK_LEFT) == LOW)
+        {
+            delay(50);
+        }
+    }
+    else if (digitalRead(PIN_STICK_RIGHT) == LOW)
+    {
+        Serial.println("RIGHT");
+
+        while (digitalRead(PIN_STICK_RIGHT) == LOW)
+        {
+            delay(50);
+        }
+    }
+    else if (digitalRead(PIN_STICK_PRESS) == LOW)
+    {
+        Serial.println("PRESS");
+        while (digitalRead(PIN_STICK_PRESS) == LOW)
+        {
+            delay(50);
+        }
+    }
+
+    if (digitalRead(PIN_BUTTON_A) == LOW)
+    {
+        Serial.println("A");
+
+        while (digitalRead(PIN_BUTTON_A) == LOW)
+        {
+            lv_task_handler(); // Call LittleVGL task handler periodically
+            lv_tick_inc(10);
+            delay(50);
+        }
+    }
+    if (digitalRead(PIN_BUTTON_B) == LOW)
+    {
+        Serial.println("B");
+
+        while (digitalRead(PIN_BUTTON_B) == LOW)
+        {
+            lv_task_handler(); // Call LittleVGL task handler periodically
+            lv_tick_inc(10);
+            delay(50);
+        }
+    }
+
+    lv_task_handler(); // Call LittleVGL task handler periodically
+    lv_tick_inc(1);
+}
